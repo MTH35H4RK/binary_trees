7737950380bc52be873758540bb313055a2cf0af
bouarichaKarim <bouaaricha.karim@gmail.com>
2023-08-31 16:32:30 -0700

    Update 4-binary_tree_is_leaf

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -4,7 +4,7 @@
  * binary_tree_is_leaf - checks if a node is a leaf
  * @node: the node to be checked
  * Return: 0 or 1.
-*/
+ */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	return (node && node->left == NULL && node->right == NULL);
